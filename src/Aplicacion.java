9abdb1dc0eb763c34442649a417882e720382d6d
Marcelo Valenzuela Rabela <valenzuelarabela13@gmail.com>
2021-04-28 20:12:07 +0200

    Proyecto finalizado

--- src/Aplicacion.java
+++ src/Aplicacion.java
@@ -1,4 +1,22 @@
 public class Aplicacion {
+    public static void main(String[] args) {
+
+
+        Animal perro = new Perro();
+
+        Perro perrito = new Perro();
+
+        Animal animalPolimorfico = new Perro();
+        animalPolimorfico = perrito;
+        animalPolimorfico = perro;
+
+        Animal gato = new Gato();
+
+        Ianimal gatito = new Gato();
+
+    }
+
+
 
 
 }
